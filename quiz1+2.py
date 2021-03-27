7b38edfe8ff346f0c56d078c4bafca2f6244e1f0
myrrari <81439260+myrrari@users.noreply.github.com>
2021-03-27 22:27:06 +0900

    Add files via upload
    행정21 조연수

---
+++ quiz1+2.py
@@ -0,0 +1,16 @@
+#1주차
+#Quiz1
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+#Quiz2
+from random import *
+study = (randrange(4,29))
+
+print("오프라인 스터디 모임 날짜는 매월 " + str(study) + "일로 선정되었습니다.")
